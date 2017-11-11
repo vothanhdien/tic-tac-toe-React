51ef67999b29fc475e56ac1345594ab47c313b76
Vo Thanh Dien <vtdien96@gmail.com>
2017-11-11 12:41:26 +0700

    render board theo size nguoi dung nhap vao

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ import './index.css';
 //     }
 // }
 var MAX = 5;
-function boardRow(props) {
+
+function Row(props) {
     return(
         <div className="board-row">
             {props.value}
@@ -25,59 +26,58 @@ function Square(props) {
         </button>
     );
 }
+
+class InputBoardSize extends React.Component{
+    render(){
+        return(
+            <div className="inputBoardSize">
+                <input type="number" name="size" id="inputSize" className="form-control" min="5" step="1" required="required" title=""/>
+                <button className="btn btn-danger" onClick={()=>this.props.onClick()}>Resize</button>
+            </div>
+        )
+    }
+}
+
+
 class Board extends React.Component {
   renderSquare(i) {
     return <Square
         value = {this.props.squares[i]}
-        onClick={() => this.props.onClick(i)} />;
+        onClick={() => this.props.onClick(i)}/>;
   }
-  renderBoardRow(val){
-      return <boardRow
-          value = {val}
+  renderRow(listSquares){
+      return <Row
+          value = {listSquares}
           />
   }
 
   render() {
-
+    let listRow = [];
+    let width = Math.sqrt(this.props.squares.length);
+    for(let i = 0; i< width; i++){
+        let listSquare = [];
+        for(let j = 0; j < width; j++){
+            listSquare.push(this.renderSquare(i*width + j));
+        }
+        listRow.push(this.renderRow(listSquare));
+    }
     return (
       <div>
-          <div className="board-row">
-              {this.renderSquare(0)}
-              {this.renderSquare(1)}
-              {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-              {this.renderSquare(3)}
-              {this.renderSquare(4)}
-              {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-              {this.renderSquare(6)}
-              {this.renderSquare(7)}
-              {this.renderSquare(8)}
-          </div>
+          {listRow}
       </div>
     );
   }
 }
 //rewrite
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
+function calculateWinner(squares, row, col) {
+    const width = squares.length
+    let listHorizon
+    // for (let i = 0; i < lines.length; i++) {
+    //     const [a, b, c] = lines[i];
+    //     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    //         return squares[a];
+    //     }
+    // }
     return null;
 }
 
@@ -86,17 +86,17 @@ class Game extends React.Component {
 
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winner = calculateWinner(current.squares, current.row, current.col);
 
     const moves = history.map((step, move) => {
-      const position = " =====  (column,row): (" + history[move].col + " , " + history[move].row +
+      const position = " =====  (row,column): (" + history[move].row + " , " + history[move].col +
       ") ***player: " + history[move].player;
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move #' + move + position :
         'Go to game start';
       return (
         <li key={move}>
-          <button className ="btn btn-primary" onClick={() => this.jumpTo(move)}>{desc + position}</button>
+          <button className ="btn btn-primary" onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
     });
@@ -121,6 +121,10 @@ class Game extends React.Component {
           </div>
           <hr/>
           <div className="game-info">
+              <InputBoardSize
+                  onClick = {()=> this.resizeButtonClick()}
+              />
+              <hr/>
               <div><h1><strong>{ status }</strong></h1></div>
             <ol>{moves}</ol>
           </div>
@@ -134,14 +138,17 @@ class Game extends React.Component {
     const current = history[this.state.stepNumber];
     const squares = current.squares.slice();
 
-    if(calculateWinner(squares) || squares[i]){
+    const width = current.length;
+    const row = (i/width + 1)|0;
+    const col = i%MAX + 1;
+    if( squares[i] || calculateWinner(squares, row, col)){
         return;
     }
     squares[i] = this.state.xIsNext? "X" : "O";
     this.setState({
         history: history.concat([{ squares: squares,
-                                    col: i%3 + 1,
-                                    row: (i/3 + 1)|0,
+                                    col: col,
+                                    row: row,
                                     player: this.state.xIsNext? "X":"O",}]),
         stepNumber : history.length,
         xIsNext:!this.state.xIsNext,
@@ -154,12 +161,29 @@ class Game extends React.Component {
     });
 
   }
-
+  resizeButtonClick(){
+      let newsize = parseInt(document.getElementById('inputSize').value,10);
+      if(newsize && newsize > 4){
+        alert(newsize);
+        MAX = newsize;
+        this.setState({
+            history :[{
+                squares: Array(Math.pow(MAX,2)).fill(null),
+                row: null,
+                col: null,
+                player: "unknow",
+            }],
+            stepNumber: 0,
+            xIsNext: true
+        })
+      }else
+          alert("vui long nhap lai size");
+  }
   constructor(props){
       super(props);
       this.state ={
           history :[{
-              squares: Array(MAX*MAX).fill(null),
+              squares: Array(Math.pow(MAX,2)).fill(null),
               row: null,
               col: null,
               player: "unknow",
